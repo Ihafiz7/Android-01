da3eae57ceedc0751a0c8af794a763f652477aa7
Hafiz <hafizalasad712@gmail.com>
2025-11-01 12:56:02 +0600

    first

---
+++ app/src/main/java/com/hafiz/android_01/MainActivity.java
@@ -0,0 +1,50 @@
+package com.hafiz.android_01;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUserName, etPassword;
+    Button btnLogin, btnSignup;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        etUserName =findViewById(R.id.phEmail);
+        etPassword = findViewById(R.id.phPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eamil = etUserName.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                System.out.println(eamil +  password);
+                Toast.makeText(MainActivity.this, "logging in ...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
